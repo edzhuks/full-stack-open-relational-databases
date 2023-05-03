715b48ea6dc5e4f8821bcbef1ae8a335675aa708
edgars <edzhuks@gmail.com>
2023-05-03 17:14:24 +0300

    part13-ex12

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -26,7 +26,7 @@ const tokenExtractor = (req, res, next) => {
 }
 
 router.get('/', async (req, res) => {
-  const blogs = await Blog.findAll()
+  const blogs = await Blog.findAll({ include: { model: User } })
   res.json(blogs)
 })
 
