aa2ee6e9711e7e58f2190d352a06969885f921b3
edgars <edzhuks@gmail.com>
2023-05-03 20:14:45 +0300

    part13-ex16

---
+++ controllers/authors.js
@@ -0,0 +1,21 @@
+const router = require('express').Router()
+const jwt = require('jsonwebtoken')
+
+const { ValidationError, Op } = require('sequelize')
+const { Blog, User } = require('../models')
+const { sequelize } = require('../util/db')
+
+router.get('/', async (req, res) => {
+  const authors = await Blog.findAll({
+    attributes: [
+      'author',
+      [sequelize.fn('COUNT', sequelize.col('id')), 'articles'],
+      [sequelize.fn('SUM', sequelize.col('likes')), 'likes'],
+    ],
+    order: [['likes', 'DESC']],
+    group: 'author',
+  })
+  res.json(authors)
+})
+
+module.exports = router
